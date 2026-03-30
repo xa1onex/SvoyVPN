65627aa5a9e343d7ab8678db6ac4fad7357eea37
macbookair <cr7dima777@gmail.com>
2026-03-30 20:57:29 +0300

    Cleanup unused and temporary scripts

--- check.py
+++
@@ -1,7 +0,0 @@
-import re
-js = open("miniapp/need/app_v77.js").read()
-html = open("miniapp/index.html").read()
-ids = set(re.findall(r"document\.getElementById\(['\"](.*?)['\"]\)", js))
-html_ids = set(re.findall(r"id=['\"](.*?)['\"]", html))
-missing = ids - html_ids
-print("Missing IDs:", missing)
