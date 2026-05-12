9149f30c7354fd901c163c4ed2250e2bcdbab5bf
macbookair <cr7dima777@gmail.com>
2026-05-12 17:45:37 +0300

    update subscription

--- bot/yookassa_client.py
+++ bot/yookassa_client.py
@@ -78,11 +78,12 @@ class YooKassaClient:
         }
         if save_payment_method:
             payment_dict["save_payment_method"] = True
+            payment_dict["payment_method_data"] = {"type": "bank_card"}
             mc = merchant_customer_id or meta.get("user_id")
             if mc:
                 payment_dict["merchant_customer_id"] = str(mc)[:200]
             logger.info(
-                "YooKassa payment with save_payment_method (привязка для автоплатежей)"
+                "YooKassa payment with save_payment_method (привязка для автоплатежей, только bank_card)"
             )
 
         try:
